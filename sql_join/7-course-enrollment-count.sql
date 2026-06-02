268fc1c3fbdc59ec85c81913f527a5c80e145e77
Hugol4ka <hugolaca12300@gmail.com>
2026-06-02 14:21:30 +0200

    Fix course_id for courses.id

--- sql_join/7-course-enrollment-count.sql
+++ sql_join/7-course-enrollment-count.sql
@@ -1,5 +1,5 @@
 SELECT courses.title AS course_title, COUNT(enrollments.student_id) AS enrollment_count
 FROM courses
 LEFT JOIN enrollments ON courses.id = enrollments.course_id
-GROUP BY course_id
+GROUP BY courses.id
 ORDER BY enrollment_count DESC, course_title ASC    
